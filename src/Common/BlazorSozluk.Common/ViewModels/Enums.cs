7cfb7bc7ef54eee2e5f4ec5c9f22bb452f5cccff
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-07-01 16:59:32 +0300

    Add project files.

---
+++ src/Common/BlazorSozluk.Common/ViewModels/Enums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Common.ViewModels
+{
+    public enum VoteType
+    {
+        None = -1, // ilgili entry'e ait herhangi bir oy kullanilmamissa
+        DownVote = 0, // ilgili entry'e ait dusuk bir oy kullanilmissa
+        UpVote = 1 // ilgili entry'e ait yuksek bir oy kullanilmissa
+    }
+}
