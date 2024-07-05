67c18f6cbbfe751f224f419444041ef6b4478236
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-07-05 16:22:11 +0300

    API altyapı çalışması, exception handle edilmesi - 11

---
+++ src/Api/WebApi/BlazorSozluk.Api.WebApi/Infrastructure/ActionFilters/ValidateModelStateFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BlazorSozluk.Api.WebApi.Infrastructure.ActionFilters
+{
+    // Sistemimiz icerisinde bir validasyon hatasi olustugu zaman calisacak olan metodumuz bu
+    public class ValidateModelStateFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var messages = context.ModelState.Values.SelectMany(x => x.Errors)
+                                                        .Select(x => !string.IsNullOrEmpty(x.ErrorMessage) ?
+                                                        x.ErrorMessage : x.Exception?.Message)
+                                                        .Distinct().ToList();
+
+                return;
+            }
+
+            await next();
+        }
+    }
+}
--- src/Api/WebApi/BlazorSozluk.Api.WebApi/Program.cs
+++ src/Api/WebApi/BlazorSozluk.Api.WebApi/Program.cs
@@ -1,5 +1,6 @@
 using BlazorSozluk.Api.Application.Extensions;
 using BlazorSozluk.Api.Infrastructure.Persistence.Extensions;
+using BlazorSozluk.Api.WebApi.Infrastructure.Extensions;
 using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,6 +20,8 @@ builder.Services
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.ConfigureAuth(builder.Configuration);
+
 builder.Services.AddApplicationRegistration();
 builder.Services.AddInfrastructureRegistration(builder.Configuration);
 
@@ -33,6 +36,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Hatalarin handle edilmesi
+app.ConfigureExceptionHandling(app.Environment.IsDevelopment());
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
