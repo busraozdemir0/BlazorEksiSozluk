bab43f5b8b3dd64f447f03a9b2e19d0f1425f795
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-07-08 14:36:31 +0300

    Ana sayfada Entry'lerin listelenmesi (Razor Componentler ile) - 15

---
+++ src/Clients/BlazorWeb/BlazorSozluk.WebApp/Infrastructure/Models/VoteClickedEventArgs.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorSozluk.WebApp.Infrastructure.Models
+{
+    public class VoteClickedEventArgs : EventArgs
+    {
+        public Guid EntryId { get; set; }
+        public bool IsUpVoteClicked { get; set; } // Yukari oka tiklandi
+        public bool UpVoteDeleted { get; set; } // Yukari oka tiklanmasi kaldirildi
+        public bool IsDownVoteClicked { get; set; }
+        public bool DownVoteDeleted { get; set; }
+    }
+}
